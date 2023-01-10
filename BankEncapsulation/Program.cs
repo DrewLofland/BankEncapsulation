7a1ec8fc212e179da8b690a0a61cb0bbbcb3dd2b
Drew Lofland <alofland25@gmail.com>
2023-01-10 11:31:46 -0500

    finished exercise

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace BankEncapsulation
+﻿using System.Globalization;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount ba = new BankAccount();
+            Console.WriteLine($"Please enter a deposit amount");
+            double depositAmount = double.Parse(Console.ReadLine());
+
+            ba.Deposit(depositAmount);
+
+            double userBalance = ba.GetBalance();
+
+            Console.WriteLine($"Current Balance: {userBalance}");
+
         }
     }
 }
